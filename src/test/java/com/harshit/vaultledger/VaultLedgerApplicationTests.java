07bb42230db5486f0379aea93965c73a04b2d51d
sharmaharshitnone <sharmaharshitnone@gmail.com>
2026-02-10 13:36:30 +0530

    feat: Initialize project structure

---
+++ src/test/java/com/harshit/vaultledger/VaultLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshit.vaultledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
